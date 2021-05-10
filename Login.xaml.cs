4016a9b4b82ba25067cc19609ae37b15881f035c
eswaribala <Parameswaribala@gmail.com>
2021-05-11 00:19:37 +0530

    Add files via upload

---
+++ Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+using SQLite;
+using StackLayoutForm.Models;
+using StackLayoutForm.Persistence;
+using StackLayoutForm.ViewModels;
+using StackLayoutForm.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayoutForm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+       
+
+        public Login()
+        {
+            // InitializeComponent();
+           
+            var vm = new LoginViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+            vm.ValidLoginPrompt += () =>
+            {
+                this.Navigation.PushAsync(new HomePage());
+            };
+            
+            InitializeComponent();
+            Email.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                vm.SubmitCommand.Execute(null);
+            };
+        }
+
+        
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new RegisterPage());
+        }
+
+        
+    }
+}
